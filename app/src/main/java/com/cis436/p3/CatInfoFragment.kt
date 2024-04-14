a3f1a6b4a9b1066fb65da27ea9a209aeaa3aad74
Aidil Najmir <102715539+aidilnajmir@users.noreply.github.com>
2024-04-14 17:08:41 -0400

    Commented CatInfoFragment.kt

--- app/src/main/java/com/cis436/p3/CatInfoFragment.kt
+++ app/src/main/java/com/cis436/p3/CatInfoFragment.kt
@@ -1,13 +1,10 @@
 package com.cis436.p3
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.cis436.p3.databinding.FragmentCatInfoBinding
 import com.squareup.picasso.Picasso
@@ -66,4 +63,3 @@ class CatInfoFragment : Fragment() {
         _binding = null
     } //end of onDestroyView
 }
-
