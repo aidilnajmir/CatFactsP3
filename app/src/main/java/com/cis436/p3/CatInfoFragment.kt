64f460e7018ee49bc38e6dc0afefc5515b4f9aef
Aidil Najmir <102715539+aidilnajmir@users.noreply.github.com>
2024-04-14 17:07:41 -0400

    Commented CatInfoFragment.kt

--- app/src/main/java/com/cis436/p3/CatInfoFragment.kt
+++ app/src/main/java/com/cis436/p3/CatInfoFragment.kt
@@ -14,6 +14,7 @@ import com.squareup.picasso.Picasso
 
 class CatInfoFragment : Fragment() {
 
+    // Data binding and ViewModel variables
     private var _binding: FragmentCatInfoBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: CatViewModel
@@ -24,37 +25,45 @@ class CatInfoFragment : Fragment() {
     ): View? {
         _binding = FragmentCatInfoBinding.inflate(inflater, container, false)
         return binding.root
-    }
+    } //end of onCreateView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity()).get(CatViewModel::class.java)
+        // ViewModel initialization associated with the parent activity
+        viewModel = ViewModelProvider(requireActivity())[CatViewModel::class.java]
+
+        // Function call to observe changes in ViewModel data
         observeViewModel()
-    }
+    } //end of onViewCreated
 
     private fun observeViewModel() {
+        // Observe changes in selectedCatName LiveData and update the TextView
         viewModel.selectedCatName.observe(viewLifecycleOwner) { name ->
             binding.tvName.text = name
         }
+        // Observe changes in selectedCatTemperament LiveData and update the TextView
         viewModel.selectedCatTemperament.observe(viewLifecycleOwner) { temperament ->
             binding.tvTemperament.text = temperament
         }
+        // Observe changes in selectedCatOrigin LiveData and update the TextView
         viewModel.selectedCatOrigin.observe(viewLifecycleOwner) { origin ->
             binding.tvOrigin.text = origin
         }
+        // Observe changes in selectedCatImageUrl LiveData and update the ImageView using Picasso
         viewModel.selectedCatImageUrl.observe(viewLifecycleOwner) { imageUrl ->
-            //Log.d("Image", imageUrl)
             if (!imageUrl.isNullOrEmpty()) {
                 Picasso.get().load(imageUrl).into(binding.imgCat)
             } else {
+                // Load a default image into the ImageView if imageUrl is null or empty
                 Picasso.get().load(R.drawable.default_image).into(binding.imgCat)
             }
         }
-    }
+    } //end if observeViewModel
 
     override fun onDestroyView() {
         super.onDestroyView()
+        // Clean up the binding object to avoid memory leaks
         _binding = null
-    }
+    } //end of onDestroyView
 }
 
